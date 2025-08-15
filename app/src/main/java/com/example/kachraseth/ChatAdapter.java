0ea40368f8ae1dc8ac3c7613d6786e41a5172c03
sidhu692004 <163583801+sidhu692004@users.noreply.github.com>
2025-08-16 02:16:43 +0530

    Initial project upload

---
+++ app/src/main/java/com/example/kachraseth/ChatAdapter.java
@@ -0,0 +1,47 @@
+package com.example.kachraseth;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ChatViewHolder> {
+
+    private List<String> chatMessages;
+
+    public ChatAdapter(List<String> chatMessages) {
+        this.chatMessages = chatMessages;
+    }
+
+    @NonNull
+    @Override
+    public ChatViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.chat_item, parent, false);
+        return new ChatViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ChatViewHolder holder, int position) {
+        String message = chatMessages.get(position);
+        holder.textViewMessage.setText(message);
+    }
+
+    @Override
+    public int getItemCount() {
+        return chatMessages.size();
+    }
+
+    static class ChatViewHolder extends RecyclerView.ViewHolder {
+        TextView textViewMessage;
+
+        public ChatViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textViewMessage = itemView.findViewById(R.id.textViewMessage);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/kachraseth/MessageAdapter.java
@@ -0,0 +1,61 @@
+package com.example.kachraseth;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.MessageViewHolder> {
+
+    private List<Message> messages;
+
+    public MessageAdapter(List<Message> messages) {
+        this.messages = messages;
+    }
+
+    @NonNull
+    @Override
+    public MessageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_message, parent, false);
+        return new MessageViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MessageViewHolder holder, int position) {
+        Message message = messages.get(position);
+        holder.bind(message);
+    }
+
+    @Override
+    public int getItemCount() {
+        return messages.size();
+    }
+
+    public void updateMessages(List<Message> newMessages) {
+        this.messages = newMessages;
+        notifyDataSetChanged();
+    }
+
+    static class MessageViewHolder extends RecyclerView.ViewHolder {
+        private TextView messageText, timestampText;
+
+        public MessageViewHolder(@NonNull View itemView) {
+            super(itemView);
+            messageText = itemView.findViewById(R.id.messageText);
+            timestampText = itemView.findViewById(R.id.timestampText);
+        }
+
+        public void bind(Message message) {
+            messageText.setText(message.getMessage());
+            timestampText.setText(new SimpleDateFormat("hh:mm a", Locale.getDefault()).format(new Date(message.getTimestamp())));
+        }
+    }
+}
